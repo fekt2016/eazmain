2cbb34d592358d78b085036dafdd1b520678d4b8
yussif <fekt2016@icloud.com>
2025-09-02 23:51:23 +0000

    hosting

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://eazworld:4000/api/v1";
+const baseURL =
+  import.meta.env.VITE_API_URL || "http://eazworld.com:4000/api/v1";
 
 // 1. Public exact path matches (any HTTP method)
 const PUBLIC_ROUTES = [
